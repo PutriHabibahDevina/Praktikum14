c694aee58266f3162229ea3b378409aea045783f
PutriHabibahDevina <putrihdm13@gmail.com>
2025-01-13 14:15:18 +0700

    Menghapus import

--- app/src/main/java/com/example/praktikum14/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum14/ui/viewmodel/PenyediaViewModel.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.praktikum14.Viewmodel.HomeViewModel
 import com.example.praktikum14.MahasiswaApp
-import com.example.praktikum14.model.Mahasiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
