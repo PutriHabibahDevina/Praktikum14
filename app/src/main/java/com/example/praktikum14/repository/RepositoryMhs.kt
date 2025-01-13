dd1f0d63a6d7d283340f044c70af561c16bb7c0a
PutriHabibahDevina <putrihdm13@gmail.com>
2025-01-13 13:23:30 +0700

    Mengganti fungsi dan parameter data mahasiswa

--- app/src/main/java/com/example/praktikum14/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum14/repository/RepositoryMhs.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMhs(): Flow<List<Mahasiswa>>
-    suspend fun getMhs(): List<Mahasiswa>
-    suspend fun updateMhs(nim:String, mahasiswa: Mahasiswa)
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
     suspend fun deleteMhs(nim: String)
 }
